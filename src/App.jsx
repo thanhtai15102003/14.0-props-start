f3ad31b32e3d7a783d3bdc6ae8b6c74e44949c48
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-11-26 18:37:25 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 
 import Header from "./components/Header/Header.jsx";
 import { myData } from "../data.js";
-import MainContent from "./components/Header/MainContent/MainContent.jsx";
+import MainContent from "./components/MainContent/MainContent.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Vui lòng click vào nút");
+  function handleClick(selectedButton) {
+        setSelectedTopic(selectedButton);
+    }
   return (
     <>
       <Header />
@@ -17,6 +24,17 @@ function App() {
             <MainContent {...myData[2]}/>
             <MainContent {...myData[3]}/>
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() =>{handleClick('Components')} }>Components</TabButton>
+            <TabButton onSelect={() =>{handleClick('JSX')} }>JSX</TabButton>
+            <TabButton onSelect={() =>{handleClick('Props')} }>Props</TabButton>
+            <TabButton onSelect={() =>{handleClick('State')} }>State</TabButton>
+          </menu>
+          {selectedTopic}
         </section>
       </main>
     </>
